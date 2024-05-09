27afd7098b4fdbe54e4b7a9d93007a4ca913e125
babinicz <babinicz@onet.pl>
2024-05-09 20:05:37 +0200

    zm

--- components/main/CookiesConsent.tsx
+++ components/main/CookiesConsent.tsx
@@ -21,13 +21,13 @@ export default function CookiesConsent() {
   return (
     <div className="fixed inset-0 bg-slate-700 bg-opacity-70 z-[5000]">
       <div className="fixed bottom-0 left-0 flex flex-col items-center sm:flex-row sm:justify-between px-4 py-8 bg-custom_gold text-custom_white">
-        <span className="text-dark text-base mr-16">
-          W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
-          Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
-          indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
-          dotyczących cookies oznacza, że będą one zamieszczane w Państwa
-          urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
-          ustawień dotyczących cookies.
+        <span className="text-dark text-base sm:mr-16 w-auto">
+          W ramach naszej witryny stosujemy pliki cookies w&nbsp;celu
+          świadczenia Państwu usług na najwyższym poziomie, w&nbsp;tym
+          w&nbsp;sposób dostosowany do indywidualnych potrzeb. Korzystanie
+          z&nbsp;witryny bez zmiany ustawień dotyczących cookies oznacza, że
+          będą one zamieszczane w Państwa urządzeniu końcowym. Możecie Państwo
+          dokonać w&nbsp;każdym czasie zmiany ustawień dotyczących cookies.
           <Link href="/privacy-policy" className="font-bold text-custom">
             Polityka prywatności
           </Link>{" "}
