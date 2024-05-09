5714905352a3be1047b9940fa5ff191c8e93943a
babinicz <babinicz@onet.pl>
2024-05-09 15:24:45 +0200

    zm metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,28 +19,37 @@ export default function RootLayout({
     <>
       <html lang="pl">
         <head>
-          <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>{metadata.title.default}</title>
-          <meta name="description" content={metadata.description} />
-          <meta name="keywords" content={metadata.keywords.join(", ")} />
-          <meta name="twitter:card" content={metadata.twitter.card} />
-          <meta
-            name="twitter:description"
-            content={metadata.twitter.description}
-          />
-          {/* inne metadane */}
-          {metadata.additionalMetaTags &&
-            metadata.additionalMetaTags.map((meta, index) => (
-              <meta
-                key={index}
-                property={meta.property}
-                content={meta.content}
-              />
-            ))}
+          <>
+            <meta charSet="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <title>{metadata.title.default}</title>
+            <meta name="description" content={metadata.description} />
+            <meta name="keywords" content={metadata.keywords.join(", ")} />
+            <meta name="twitter:card" content={metadata.twitter.card} />
+            <meta
+              name="twitter:description"
+              content={metadata.twitter.description}
+            />
+            {/* Dodaj odnośnik do głównego obrazka Twittera */}
+            <meta name="twitter:image" content={metadata.twitter.images[0]} />
+            {/* Dodaj odnośnik do głównego obrazka Open Graph */}
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+            {/* inne metadane */}
+            {metadata.additionalMetaTags &&
+              metadata.additionalMetaTags.map((meta, index) => (
+                <meta
+                  key={index}
+                  property={meta.property}
+                  content={meta.content}
+                />
+              ))}
+          </>
         </head>
         <body
           className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
